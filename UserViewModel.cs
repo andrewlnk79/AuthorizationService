a4764b36db31622870485957f907e97ccb89aedc
LinnikA <a@linnik-1.ru>
2025-03-03 10:46:11 +0300

    added class UserViewModel

---
+++ UserViewModel.cs
@@ -0,0 +1,32 @@
+using System.Net.Mail;
+
+namespace AuthorizationService;
+
+public class UserViewModel
+{
+    public UserViewModel(User user)
+    {
+        Id = user.Id;
+
+        FullName = GetFullName(user.FirstName, user.LastName);
+        FromRussia = GetFromRussiaValue(user.Email);
+    }
+
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+    public bool FromRussia { get; set; }
+
+    public string GetFullName(string firstName, string lastName)
+    {
+        return string.Concat(firstName + " " + lastName);
+    }
+
+    public bool GetFromRussiaValue(string email)
+    {
+        var mailAddress = new MailAddress(email);
+
+        if (mailAddress.Host.Contains(".ru"))
+            return true;
+        return false;
+    }
+}
