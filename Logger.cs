5471bea57f6329644feaddbb6462ad87f9d4ea33
LinnikA <a@linnik-1.ru>
2025-03-03 10:27:50 +0300

    added logg logic

--- Logger.cs
+++ Logger.cs
@@ -2,13 +2,48 @@ namespace AuthorizationService;
 
 public class Logger : Ilogger
 {
+    private readonly ReaderWriterLockSlim lock_ = new();
+
+    public Logger()
+    {
+        logDirectory = AppDomain.CurrentDomain.BaseDirectory + @"/_logs/" + DateTime.Now.ToString("dd-MM-yy HH-mm-ss") +
+                       @"/";
+
+        if (!Directory.Exists(logDirectory))
+            Directory.CreateDirectory(logDirectory);
+    }
+
+    private string logDirectory { get; }
+
     public void WriteEvent(string eventMessage)
     {
-        Console.WriteLine(eventMessage);
+        lock_.EnterWriteLock();
+        try
+        {
+            using (var writer = new StreamWriter(logDirectory + "events.txt", true))
+            {
+                writer.WriteLine(eventMessage);
+            }
+        }
+        finally
+        {
+            lock_.ExitWriteLock();
+        }
     }
 
     public void WriteError(string errorMessage)
     {
-        Console.WriteLine(errorMessage);
+        lock_.EnterWriteLock();
+        try
+        {
+            using (var writer = new StreamWriter("errors.txt", true))
+            {
+                writer.WriteLine(errorMessage);
+            }
+        }
+        finally
+        {
+            lock_.ExitWriteLock();
+        }
     }
 }
